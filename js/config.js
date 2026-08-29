39ff99ca9773e3b1217c4620a0fc9db568feb7ba
oumar zeini <oumaribnzeini7@gmail.com>
2026-08-29 19:54:00 +0100

    Migrated to Vite

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-const supabaseUrl = "https://xytbxpjfjdwmdcffvvqh.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5dGJ4cGpmamR3bWRjZmZ2dnFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwMzM1OTQsImV4cCI6MjA3NTYwOTU5NH0.TqJdNgYNmIH7GiQot-RfTCF6e1m7iLDivBnnt6u-iAU";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY,
+);
 export default supabase;
